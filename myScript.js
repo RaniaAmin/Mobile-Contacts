39f0c406fdf3fc60633410421cfb4fe4493b57f0
RaniaAmin <rania.amin2020@gmail.com>
2021-12-18 16:12:06 +0200

    working on removing contact function

--- myScript.js
+++ myScript.js
@@ -3,32 +3,24 @@ var contacts=[
     {name:"rania",phoneNumber:"32534652"},
     {name:"mohammed", phoneNumber:"2354362663"}
 ]
-console.log(contacts[0]);
+
 document.addEventListener("load",displayList());
+////////////////////displayList function//////////////////////////
 function displayList() {
     var list=document.getElementById("contactList");
     list.innerHTML="";
     var container=document.getElementById("listContainer");
-
-    // var newList=document.createElement("ul");
-contacts.forEach((contact)=>{
+contacts.forEach((contact,id)=>{
     var listItem=document.createElement("li");
-    listItem.textContent=contact.name+" "+contact.phoneNumber;
+   // listItem.textContent=contact.name+" "+contact.phoneNumber+"<span>Remove</span>";
+    listItem.innerHTML=contact.name+" "+contact.phoneNumber+" "+id+"<span><a href='' onclick='removeFromList("+id+")'>Remove</a></span>";
     list.appendChild(listItem);
 });
-// console.log(newList);
+}////end of displayList
 
-console.log(list);
-// console.log(list.nodeType);
-// container.removeChild(list);
-// container.appendChild(newList);
-}//end of displayList
+/////////////////////addContact function//////////////////////////
 function addContact(e){
     e.preventDefault();
-    console.log(e);
-    
-    // const inputName=document.getElementById("addName");
-    // const inputPhone=document.getElementById("addPhone");
     const inputName=document.forms["addContactForm"]["name"].value;
     const inputPhone=document.forms["addContactForm"]["phone"].value;
     var newContact={name:inputName,phoneNumber:inputPhone}
@@ -37,9 +29,14 @@ function addContact(e){
     document.forms["addContactForm"]["name"].value="";
     document.forms["addContactForm"]["phone"].value="";
     displayList();
-    
-    
+}////end of addContact
 
+///////////////////////removeFromList///////////////////////////
+function removeFromList(e,id) {
+    e.preventDefault();
+    alert("hey"+id);
 }
+
+
 
 
