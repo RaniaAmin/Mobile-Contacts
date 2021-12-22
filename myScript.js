afce3836cf53918e2859f7e69835fd2be3bd3cc4
RaniaAmin <rania.amin2020@gmail.com>
2021-12-22 19:16:33 +0200

    search function is added

--- myScript.js
+++ myScript.js
@@ -5,7 +5,7 @@ var contacts=[
 ]
 var activeContactId=null;
 
-document.addEventListener("load",displayList());
+document.addEventListener("load",displayList(contacts));
 
 
 document.getElementById("closeEdit").addEventListener("click",()=>{
@@ -13,11 +13,10 @@ document.getElementById("closeEdit").addEventListener("click",()=>{
     document.getElementById("editBlock").style.display="none";
 })
 ////////////////////displayList function//////////////////////////
-function displayList() {
+function displayList(currentContactList) {
     var list=document.getElementById("contactList");
     list.innerHTML="";
-    var container=document.getElementById("listContainer");
-contacts.forEach((contact,id)=>{
+    currentContactList.forEach((contact,id)=>{
     var listItem=document.createElement("li");    
     listItem.innerHTML=contact.name+" "+contact.phoneNumber+" "+`<span><a href='#' onclick='removeFromList(${contact.id})'>Remove</a><a href='#' id="edit" onclick='setContactId(${contact.id})'>Edit</a></span>`;
     list.appendChild(listItem);
@@ -29,21 +28,26 @@ function addContact(e){
     e.preventDefault();
     const inputName=document.forms["addContactForm"]["name"].value;
     const inputPhone=document.forms["addContactForm"]["phone"].value;
-    
     var newContact={id:contacts.length,name:inputName,phoneNumber:inputPhone}
     contacts=[...contacts,newContact]
     
     document.forms["addContactForm"]["name"].value="";
     document.forms["addContactForm"]["phone"].value="";
-    displayList();
+    displayList(contacts);
 }////end of addContact
 
 ///////////////////////removeFromList///////////////////////////
 function removeFromList(id) {
     contacts=contacts.filter((contact,i)=>(i !==id));
     console.log(contacts);
-    displayList();
+    displayList(contacts);
 }/////end of removeFromList
+
+/////////////////////////editContactList///////////////////////
+function setContactId(id) {
+    document.getElementById("editBlock").style.display="block";
+    activeContactId=id;
+}/////end of setContactId
 
 //////////////////////editContactList//////////////////////////
 function editContactList(event) {
@@ -57,17 +61,27 @@ function editContactList(event) {
          const newInputPhone=editForm["phone"].value;
          contacts[id]={id,name:newInputName,phoneNumber:newInputPhone}
          document.getElementById("editBlock").style.display="none";
-         displayList();
-         
-         
-        
-        
+         displayList(contacts);
+}/////end of editContactList
+
+function searchContactList(event) {
+    event.preventDefault();
+    var searchContact=document.getElementById("search").value;
+    // console.log(searchContact.value);
+    
+    var searchedList=contacts.filter((contact,i)=>{
+        console.log("any contact",contact);
+        if (contact.name.includes(searchContact)||contact.phoneNumber.includes(searchContact)){
+            console.log(contact);
+            return contact;
+        }
+    });
+    console.log("searchedList",searchedList);
+    displayList(searchedList);
+
+    
 }
-/////////////////////////editContactList///////////////////////
-function setContactId(id) {
-    document.getElementById("editBlock").style.display="block";
-    activeContactId=id;
-}
+
 
 
 
